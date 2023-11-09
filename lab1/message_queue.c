82192c387b8ef48cf306aeed254678a4124872da
kenandizdarevic <kenan.dizdareviic@gmail.com>
2023-11-09 20:07:52 +0100

    Lab 1 finished

--- lab1/message_queue.c
+++ lab1/message_queue.c
@@ -1,68 +1,98 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include<stdlib.h>
-#include<sys/wait.h>
-#include<unistd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 #include <mqueue.h>
 
-int main() {
-    int BUFFER_SIZE = 25;
-    char write_msg[] = "this is from the child\n";
-    char read_msg[BUFFER_SIZE];
-    int stat;
-    int fd[2];
+#define MAX_SIZE 10000
 
-    int MAX_NUM_MSG = 1000;
-    int MAX_SIZE = 1000;
+int word_count(char buffer[]){
+    int word_count = 0;
+    char *token = strtok(buffer, " \t\n");
+
+    while (token != NULL) {
+        word_count++;
+        token = strtok(NULL, " \t\n");
+    }
+    return word_count;
+}
+
+void child_process() {
+    // Open the message queue for reading
+    mqd_t mq = mq_open("/my_queue", O_RDONLY);
+
+    if (mq == (mqd_t)-1) {
+        perror("mq_open");
+        exit(EXIT_FAILURE);
+    }
 
     char buffer[MAX_SIZE];
-    char read[MAX_SIZE];
 
-    // Message queue
-    char *my_mq = "/mymq";
-    mqd_t mqd;
+    // Receive message from the message queue
+    ssize_t received = mq_receive(mq, buffer, 10000, NULL);
 
-    struct mq_attr attr = {
-        .mq_maxmsg = MAX_NUM_MSG,
-        .mq_msgsize = MAX_SIZE
-    };
-    
-    pipe(fd); // Create the pipe
-
-    switch (fork()) {
-        case -1: // fork error
-            break;
-        case 0: // Producer
-            FILE *file = fopen("message.txt", "r");
-            
-            fgets(buffer, MAX_SIZE, file);
-            
-
-            // Open the global message queue
-            mqd = mq_open(my_mq, O_CREAT | O_RDWR, 0666 ,&attr);
-
-            // Write file to the message queue
-            
-           // printf("Message: %s\n", buffer);
-            // Close file and message queue
-            fclose(file);
-            mq_close(mqd);
-            exit(1);
-            break;
-        default: // Consumer
-            wait(&stat);
-            if(WIFEXITED(stat)) {
-                printf("Message: %s\n", read);
-                mqd = mq_open(my_mq, O_RDONLY);
-                mq_receive(mqd, read, MAX_NUM_MSG, NULL);
-                printf("Message: %s\n", read);
-                mq_close(mqd);
-            }
-            
-            break;
+    if (received == -1) {
+        perror("mq_receive");
+        exit(EXIT_FAILURE);
     }
-    return 0;
+
+    printf("Number of words: %d\n", word_count(buffer));
+
+    // Close the message queue
+    mq_close(mq);
+    exit(EXIT_SUCCESS);
+}
+
+void parent_process() {
+    // Open the message queue for writing
+    mqd_t mq = mq_open("/my_queue", O_WRONLY | O_CREAT, 0644, NULL);
+
+    if (mq == (mqd_t)-1) {
+        perror("mq_open");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen("message.txt", "r");
+
+    if (file == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the content of the file
+    char buffer[MAX_SIZE];
+    size_t bytesRead = fread(buffer, 1, MAX_SIZE, file);
+
+    // Close the file
+    fclose(file);
+
+    // Send the content to the message queue
+    if (mq_send(mq, buffer, bytesRead, 0) == -1) {
+        perror("mq_send");
+        exit(EXIT_FAILURE);
+    }
+
+    // Close the message queue
+    mq_close(mq);
+}
+
+int main() {
+    switch (fork())
+    {
+    case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+        break;
+    case 0:
+        child_process();
+        break;
+    
+    default:
+        parent_process();
+        wait(NULL);
+        break;
+    }
 }
