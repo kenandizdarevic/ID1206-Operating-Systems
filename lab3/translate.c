126a8d7d4ff26c591b6a04f0bf0b1d2eadf6c572
kenandizdarevic <kenan.dizdareviic@gmail.com>
2023-11-25 17:06:21 +0100

    Add specific functions

--- lab3/translate.c
+++ lab3/translate.c
@@ -1,37 +1,77 @@
 #include <stdio.h>
 
+#define PAGE_SIZE 256
+#define PAGE_TABLE_SIZE 256
+
 const int pageOffsetMask = 0xFF;
 const int pageNumberMask = 0xFF00;
 
+// Page table has 256 entries.
+int pageTable[PAGE_TABLE_SIZE];
 
-// Read from addresses.txt, containg 32-bit integers.
-// The numbers are representing logical addresses.
-// Need to mask the rightmost 16 bits of each address.
-// 8-bits page number, 8-bits page offset.
 
-// Other specifics:
-// 16 entries in TLB
-// 256 entries in PT
-// Page size 256 bytes
-// 256 frames in physical memory
-// Frame size 256 bytes
-// Physical memory of 65 536 bytes
 
 /// @brief Extracts bits 1-8, which is the page offset.
-/// @param address 
+/// @param int address 
 /// @return int pageOffset
-int pageOffset(int address) {
-    return address & pageOffsetMask;
+int extractPageOffset(int logicalAddress) {
+    return logicalAddress & pageOffsetMask;
 }
 
 /// @brief Extracts bits 9-16, which is the page number.
-/// @param address 
+/// @param int address 
 /// @return int pageNumber
-int pageNumber(int address) {
-    return (address & pageNumberMask) >> 8;
+int extractPageNumber(int logicalAddress) {
+    return (logicalAddress & pageNumberMask) >> 8;
 }
 
-int main() {
+void initPageTable() {
+    for(int i = 0; i < PAGE_TABLE_SIZE; i++) {
+        pageTable[i] = -1;
+    }
+}
+
+/// @brief 
+/// @param int logicalAddress 
+/// @param FILE *disk 
+void handlePageFault(int logicalAddress, FILE *disk) {
+
+}
+
+/// @brief Translates logical address to physical address.
+/// @brief First check TLB for page number, if miss check page table.
+/// @brief Update TLB with FIFO replacement strategy.
+/// @param int logicalAddress 
+/// @param FILE *disk
+/// @return 
+int translateAddress(int logicalAddress, FILE *disk) {
+    /*
+    * 2. Consult the TLB.
+    * 3. If TLB hit, obtain frame number from TLB.
+    * 4. If TLB miss, consult page table.
+    * 5. Frame number obtained or handle page fault.
+    */
+
+   // Extract desired bits from logical address.
+   int pageOffset = extractPageOffset(logicalAddress);
+   int pageNumber = extractPageNumber(logicalAddress);
+
+   if(pageTable[pageNumber] == -1) {
+    handlePageFault(pageNumber, disk);
+   }
+
+
+    // Consult TLB, implement after page table.
+
+    // Consult page table
+    
+}
+
+int main(int argc, char *argv[]) {
+    FILE *addresses = fopen(argv[1], "r");
+    FILE *disk = fopen("BACKING_STORE.bin", "rb");
+
+    initPageTable();
 
     return 0;
 }
